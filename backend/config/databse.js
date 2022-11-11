35b7b0094d2e9bc4a0d9ca01d0068b347ae3c7a1
Zain <85700442+zayn-shayq@users.noreply.github.com>
2022-11-11 18:23:21 +0500

    DB Update

--- backend/config/databse.js
+++ backend/config/databse.js
@@ -1,6 +1,6 @@
 import { connect } from "mongoose";
  const connectDatabase = async () =>{
-    const URL='mongodb+srv://SemesterProject:SemesterProject@scd.0ajgjun.mongodb.net/?retryWrites=true&w=majority';
+    const URL='mongodb+srv://Project:123890@cluster0.1nsbppb.mongodb.net/?retryWrites=true&w=majority';
     try {
         await connect(URL);
         console.log("Database connected successfully")
