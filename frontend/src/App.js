c630540d2b342ed7501293c1fc318b5ef4b16bd6
i202417 <117226868+i202417@users.noreply.github.com>
2022-11-20 19:39:18 +0500

    Bootstrap
    adding bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,44 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-    <div>
-      <header>
-        <Link to="/">amazona</Link>
-      </header>
-      <main>
-        <Routes>
-          <Route path="/product/:slug" element={<ProductScreen />} />
-          <Route path="/" element={<HomeScreen />} />
-        </Routes>
-      </main>
-    </div>
-  </BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>
+                <img
+                  src="/images/brand.png"
+                  width="150"
+                  height="40"
+                  className="d-inline-block align-top"
+                  alt="React Bootstrap logo"
+                />  
+                  </Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container className= "mt-3">
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,10 @@
-//import { useEffect, useState } from 'react';
 import { useEffect, useReducer} from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
 import logger from 'use-reducer-logger';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from '../components/Product';
+import { Helmet } from 'react-helmet-async';
 // import data from '../data';
 
 const reducer = (state, action) => {
@@ -17,9 +19,7 @@ const reducer = (state, action) => {
       return state;
   }
 };
-
 function HomeScreen() {
- //  const [products, setProducts] = useState([]);
   const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
     products: [],
     loading: true,
@@ -28,8 +28,6 @@ function HomeScreen() {
   // const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-    //  const result = await axios.get('/api/products');
-     // setProducts(result.data);
       dispatch({ type: 'FETCH_REQUEST' });
       try {
         const result = await axios.get('/api/products');
@@ -37,42 +35,29 @@ function HomeScreen() {
       } catch (err) {
         dispatch({ type: 'FETCH_FAIL', payload: err.message });
       }
-
       // setProducts(result.data);
     };
     fetchData();
   }, []);
   return (
     <div>
+        <Helmet>
+        <title>Amazona</title>
+      </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
-        {/* {products.map((product) => (
-          <div className="product" key={product.slug}>
-            <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-            </Link>
-            <div className="product-info"> */}
         {loading ? (
           <div>Loading...</div>
         ) : error ? (
           <div>{error}</div>
         ) : (
-          products.map((product) => (
-            <div className="product" key={product.slug}>
-              <Link to={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </Link>
-              <div className="product-info">
-                <Link to={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </Link>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))
+          <Row>
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
         )}
       </div>
     </div>
