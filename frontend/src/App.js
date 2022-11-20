b6cf55447ea19169425a8b6c4bc9a2dee9702c05
i202417 <117226868+i202417@users.noreply.github.com>
2022-11-20 14:28:07 +0500

    Dummy LIst producting in froontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import { render } from "react-dom";
-import Header from "./component/layout/Header/Header"
-import Footer from "./component/layout/Footer/Footer"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Routes} from "react-router-dom";
-import WebFont from "webfontloader"
-import React from 'react';
-import Home from "./component/Home/Home"
-
+import data from './data';
 
 function App() {
-
-
-  
-  React.useEffect(() => {
-    WebFont.load({
-      google: {
-        families: ["Roboto", "Droid Sans", "Chilanka"],
-      },
-    });
-
-    //store.dispatch(loadUser());
-
-    //getStripeApiKey();
-  }, []);
-
-
-
-
   return (
-    
-    <Router>
-
-      <Header />
-      
-      <Footer />
-
-    </Router>
-    
-
-    
-
-
-
-
-
-
-
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+        <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
   );
 }
 
