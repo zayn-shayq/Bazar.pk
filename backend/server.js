9d72f60e2878194514b08356a985bb6e06023396
i202417 <117226868+i202417@users.noreply.github.com>
2022-11-20 17:25:11 +0500

    Routing & connection with backend
    Auther: Syed Muhammad Ammar
    1. Rouing
    2. Backend connection with front end
    3. fetch products
    4. Manage states bu hook

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,13 @@
-import app from "./app.js";
-import connectDatabase from "./config/databse.js";
-const PORT=8000;
+import express from 'express';
+import data from './data.js';
 
+const app = express();
+// test
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
 
-process.on("uncaughtException", (err) => {
-    console.log(`Error: ${err.message}`);
-    console.log(`Shutting down the server due to Uncaught Exception`);
-    process.exit(1);
-  });
-
- connectDatabase();
-// const server = app.listen(process.env.PORT, () => {
-//     console.log(`Server is working on http://localhost:${PORT}`);
-//   });
-const server= app.listen(PORT,()=> console.log(`Server is running on http://localhost:${PORT}`))
-
-  process.on("unhandledRejection", (err) => {
-    console.log(`Error: ${err.message}`);
-    console.log(`Shutting down the server due to Unhandled Promise Rejection`);
-  
-    server.close(() => {
-      process.exit(1);
-    });
-  });
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
